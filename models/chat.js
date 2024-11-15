eefd3ffdb37710c57e24aade2b9a818960e81f94
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-11-15 15:15:16 +0530

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -11,11 +11,11 @@ const chatSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
-  // message: {
-  //     type: String,
-  //     required: true,
+ message: {
+     type: String,
+     required: true,
 
-  // },
+   },
   isRead: {
     type: Boolean,
     default: false,
